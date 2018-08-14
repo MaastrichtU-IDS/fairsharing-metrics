78117e435ca3815cac3ae05947b47b0e143afe34
vemonet <vincent.emonet@gmail.com>
2018-08-14 16:01:28 +0200

    now generate a genericly named output file: fairsharing.nt and a duplicate with a timestamp

--- FAIRsharing/scrapper_metrics.py
+++ FAIRsharing/scrapper_metrics.py
@@ -158,6 +158,8 @@ def serialize_file(file, format='ttl'):
 
 #format_file = sys.arg[2]
 
-#write = '/data/'+str(metrics['title'][:8]) + str(timestarted) + '.nt'
+write_timestamp = '/data/'+str(metrics['title'][:8]) + str(timestarted) + '.nt'
+serialize_file(write_timestamp, 'nt')
+
 write = '/data/fairsharing.nt'
 serialize_file(write, 'nt')
